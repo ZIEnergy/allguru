f68112202c440b99ae6dc832d4b2eced2a30e3ee
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-16 02:02:17 +0300

    tested on devices

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,4 +1,5 @@
-$('.header__menu-button').click(function() {
+$('.header__menu-button').click(function(e) {
+  e.preventDefault();
   $(this).toggleClass('header__menu-button--active');
   $('.header__content').fadeToggle('fast').css('display', 'flex');
 });
